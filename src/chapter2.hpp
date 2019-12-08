384c4d33d52c9f05af62ca0e9d620d479347f61f
Peefy <xpf6677@163.com>
2019-12-08 21:22:02 +0800

    Update

--- src/chapter2.hpp
+++ src/chapter2.hpp
@@ -6,17 +6,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define NONE  256
+#define NUM   257
+
+#define CHAR_TO_NUM(c)   (c - '0')   // 字符数字转整型数字
+#define IS_ALPHA(c)   ('a' <= c && 'c' <= 'z') || ('A' <= c && 'c' <= 'Z') //是否是字母
+#define IS_IDEN_CHAR(c) (IS_ALPHA(c) || (c == '_'))   // 是否是标识符词素
+
 static char lookahead;
+static int tokenval = NONE;
+static int lineno = 1;
 
-// 是否是字母 
-inline bool isalpha(char c) {
-    return ('a' <= c && 'c' <= 'z') || ('A' <= c && 'c' <= 'Z');
-}
 
-// 是否是标识符词素
-inline bool isidenfierchar(char c) {
-    return isalpha(c) || c == '_';
-}
 
 void expr() {
     term();
@@ -65,16 +66,59 @@ void term() {
         putchar(lookahead);
         match(lookahead);
     }
-    else 
+    else {
         error();
+    }       
 }
 
 void match(char c) {
-
+    lookahead == c ? lookahead = getchar() : error();
 }
 
 void error() {
-
+    // 打印错误信息
+    printf("syntax error\n");
+    // 停止
+    exit(1);
 }
+
+void factor() {
+    if (lookahead == '(') {
+        match('('); expr(); match(')');
+    }
+    else if (lookahead == NUM) {
+        printf(" %d ", tokenval); match(NUM);
+    }
+    else {
+        error();
+    }
+}
+
+int lexan() {
+    int t;
+    while (1) { 
+        // 从输入流中获取一个字符的ASCII码
+        t = getchar();
+        // 去除空格和制表符
+        if (t == ' ' || t == '\t')
+            ;
+        // 换行符
+        else if (t == '\n' || t == '\r')
+            lineno += 1;
+        else if (isdigit(t)) {
+            tokenval = CHAR_TO_NUM(t);
+            t = getchar();
+            while (isdigit(t)) {
+                tokenval = tokenval * 10 + CHAR_TO_NUM(t);
+            }    
+            ungetc(t, stdin);
+            return NUM;
+        } 
+        else { 
+            tokenval = NONE;
+            return t;
+        } 
+    }  
+} 
 
 #endif
