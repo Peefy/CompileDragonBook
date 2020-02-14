5bd16bb2423411b91fe9718cf981545e7de0c706
Peefy <xpf6677@163.com>
2020-02-14 18:19:37 +0800

    Update

--- scripts/readjson.py
+++ scripts/readjson.py
@@ -1,6 +1,21 @@
 
 import json
 
+# TODO：MyClass
+class MyClass:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def funcname(**arg):
+        pass
+
+    def __prifunc(self):
+        pass
+
+def _prifunc():
+    pass
+
 if __name__ == "__main__":
     with open('./scripts/ast.json') as fd:
         obj = json.load(fd)
